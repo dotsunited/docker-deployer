b27e52f1b832932bae488bc489ba396d302a3f6c
Jonas Regner <jonas.regner@dotsunited.de>
2018-01-12 11:31:40 +0100

    add docker-entrypoint and add ssh-key support

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,15 @@ MAINTAINER dotsunited <info@dotsunited.de>
 ENV DEPLOYER_VERSION=6.0.5
 
 RUN apk update --no-cache && \
-    apk add curl --no-cache
+    apk add bash curl openssh-client tini --no-cache
 
 RUN curl -LO https://deployer.org/releases/v$DEPLOYER_VERSION/deployer.phar && \
     mv deployer.phar /usr/local/bin/dep && \
     chmod +x /usr/local/bin/dep
 
-ENTRYPOINT ["dep"]
-CMD ["--version"]
+COPY docker-entrypoint.sh /usr/local/bin/docker-entrypoint
+
+RUN chmod +x /usr/local/bin/docker-entrypoint
+
+ENTRYPOINT ["tini", "--", "docker-entrypoint"]
+
---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$SSH_PRIVATE_KEY" != "" ]; then
+    eval $(ssh-agent -s)
+    echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+fi
+
+# For Docker builds disable host key checking. Be aware that by adding that
+# you are susceptible to man-in-the-middle attacks.
+# WARNING: Use this only with the Docker executor, if you use it with shell
+# you will overwrite your user's SSH config.
+if [ -f "/.dockerenv" ]; then
+    mkdir -p ~/.ssh
+    echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+fi
+
+
+exec /usr/local/bin/dep "$@"
